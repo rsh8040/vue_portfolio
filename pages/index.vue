96972757378d43fd2d63c564b69df89f4c765127
임수현 <shlim@blatform.co.kr>
2023-04-16 16:15:21 +0900

    메일전송 오류메세지 추가구현

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@
                     <input type="text" name="message" id="message" v-model="message" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder=" text your message">
                 </div>
             </div>
-            <button @click="send()" class="text-right">SEND MESSAGE</button>
+            <button  @click="send()" class="text-right">SEND MESSAGE</button>
         </div>
     </div>
     
@@ -89,16 +89,104 @@
             <div>© 2023. Elly. All rights reserved.</div>
         </div>
     </footer>
+
+    <TransitionRoot as="template" :show="sendMailModalOpen">
+    <Dialog as="div" class="relative z-10" @close="sendMailModalOpen = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <div>
+                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+                </div>
+                <div class="mt-3 text-center sm:mt-5">
+                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">메일 전송완료!</DialogTitle>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">Elly에게 메일을 보내주셔서 감사합니다</p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-6">
+                <button type="button" class="inline-flex w-full justify-center rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600" @click="sendMailModalOpen = false">확인</button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
+  <TransitionRoot as="template" :show="sendMailModalFailOpen">
+    <Dialog as="div" class="relative z-10" @close="sendMailModalOpen = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <div>
+                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
+                    <ExclamationTriangleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+                </div>
+                <div class="mt-3 text-center sm:mt-5">
+                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">메일 전송실패</DialogTitle>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">{{ errorMessage }}</p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-6">
+                <button type="button" class="inline-flex w-full justify-center rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600" @click="sendMailModalFailOpen = false">확인</button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+  
 </template>
 
 <script setup>
 import axios from 'axios'
+import { ref } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { CheckIcon } from '@heroicons/vue/24/outline'
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline'
 
 const name = ref('')
 const email = ref('')
 const message = ref('')
 
+const sendMailModalOpen = ref(false)
+const sendMailModalFailOpen = ref(false)
+const errorMessage = ref('')
+
 async function send() {
+
+    if(!name.value) {
+        sendMailModalFailOpen.value = true
+        errorMessage.value = '이름을 입력해주세요'
+        return false
+    }
+    if(!email.value) {
+        sendMailModalFailOpen.value = true
+        errorMessage.value =  '이메일을 입력해주세요'
+        return false
+    }
+    if(!message.value) {
+        sendMailModalFailOpen.value = true
+        errorMessage.value = '보낼메세지를 입력해주세요'
+        return false
+    }
+
     axios
     .post('https://prod-30.koreacentral.logic.azure.com:443/workflows/5dae9090695a4e5988d538d33478f417/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=oTDJi5_Z7WhB1nS-K2Vm4CLQYbO-Hmq9FJbDqqaXqmU', 
     {
@@ -107,5 +195,8 @@ async function send() {
         content: message.value
     })
     .then((response) => console.log(response))
+    .catch((error) => console.log(error.response))
+    
+    sendMailModalOpen.value = true
 }
 </script>
